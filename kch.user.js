0f125fbc98d3d40d2dafd4ab786443fad2e8878b
Sly <sziszko0129@gmail.com>
2022-09-11 22:24:52 +0200

    course counter

--- kch.user.js
+++ kch.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kmooc Course Hider
 // @namespace    http://tampermonkey.net/
-// @version      0.3.5
+// @version      0.4.3
 // @updateURL    https://raw.githubusercontent.com/balintSly/kcf/master/kch.user.js
 // @downloadURL  https://raw.githubusercontent.com/balintSly/kcf/master/kch.user.js
 // @description  Hides old courses
@@ -11,6 +11,7 @@
 // @grant        none
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js
 // ==/UserScript==
+
 let months=new Object();
 months['jan']="1";
 months['feb']="2";
@@ -34,20 +35,21 @@ function initScript() {
     let path=window.location.pathname;
     if (path=="/my-courses"){
         let html=document.getElementsByClassName("ui basic segment")[0].innerHTML;
+        let counterDiv="<div id='counter_div'></div>";
         document.getElementsByClassName("ui basic segment")[0].innerHTML="<button id='hide_btn'>Hide Old</button>";
-        document.getElementsByClassName("ui basic segment")[0].innerHTML+="<button id='show_btn'>Show All</button>"+html;
+        document.getElementsByClassName("ui basic segment")[0].innerHTML+="<button id='show_btn'>Show All</button>"+counterDiv+html;
         document.getElementById('show_btn').style.display="none";
 
         document.getElementById("hide_btn").addEventListener("click", hide);
         document.getElementById("show_btn").addEventListener("click", show);
     }
-
 }
 function hide(){
     document.getElementById('show_btn').style.display="block";
     document.getElementById('hide_btn').style.display="none";
     let item_con=document.getElementsByClassName("ui divided items")[0];
     let divs=item_con.children;
+    let courseCount=divs.length;
     for (let i = 0; i < divs.length; i++) {
         let now=new Date().toLocaleString().replace(" ","").replace(" ","").split(" ")[0].slice(0, -1).replace(".0",".").replace(".0",".");
         let courseInfo=divs[i].getElementsByClassName("item");
@@ -58,15 +60,19 @@ function hide(){
         if (creditEnd.split('.')[0]<now.split('.')[0])
         {
             divs[i].style.display="none";
+            courseCount--;
         }
-        else if(creditEnd.split('.')[1]<now.split('.')[1])
+        else if(creditEnd.split('.')[0]==now.split('.')[0] && creditEnd.split('.')[1]<now.split('.')[1])
         {
             divs[i].style.display="none";
+            courseCount--;
         }
-        else if(creditEnd.split('.')[1]<now.split('.')[1])
+        else if(creditEnd.split('.')[0]==now.split('.')[0] && creditEnd.split('.')[1]==now.split('.')[1] && creditEnd.split('.')[2]<now.split('.')[2])
         {
             divs[i].style.display="none";
+            courseCount--;
         }
+        document.getElementById("counter_div").innerHTML="<h2>Aktuális kurzusaim: "+courseCount+"</h2>";
     }
 }
 function show() {
